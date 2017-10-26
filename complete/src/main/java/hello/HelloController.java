820a2cfb953e5944ebf293d7e34cdf416a7fa3a1
ezbery <brenin0204@gmail.com>
2017-10-26 13:23:58 +0200

    testing other http methods

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
 
+    @PostMapping("/{variable}")
+    public String index2(@PathVariable String variable) {
+        return "response: " + variable;
+    }
 
     @GetMapping("/test")
     public String txt() {
